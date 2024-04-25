28e2fb79ce2a66503d7fe45ca7ba285cb2ff8c0c
Kshitijv24 <kshitijv24@gmail.com>
2024-04-25 10:10:42 +0530

    Modified how we take inputs from mouse for player movement
    and fixed an issue with the camera glitch

--- RPG-Game-Project/Assets/Scripts/FollowCamera.cs
+++ RPG-Game-Project/Assets/Scripts/FollowCamera.cs
@@ -6,7 +6,7 @@ public class FollowCamera : MonoBehaviour
 {
 	[SerializeField] Transform player;
 
-    private void Update()
+    private void LateUpdate()
     {
         transform.position = player.position;
     }
--- RPG-Game-Project/Assets/Scripts/PlayerMovement.cs
+++ RPG-Game-Project/Assets/Scripts/PlayerMovement.cs
@@ -22,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButton(0))
             MoveToMousePosition();
 
         UpdateAnimator();
