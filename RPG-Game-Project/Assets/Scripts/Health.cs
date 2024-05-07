11bb454dd5e910c17e157e5aa906566f553fd0a1
Kshitijv24 <kshitijv24@gmail.com>
2024-05-07 09:15:12 +0530

    Added Player and Enemy Death Animation and triggering it in the health script.

--- RPG-Game-Project/Assets/Scripts/Health.cs
+++ RPG-Game-Project/Assets/Scripts/Health.cs
@@ -6,12 +6,28 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
-        [SerializeField] float health = 20f;
+        [SerializeField] float healthPoints = 20f;
+
+        Animator animator;
+        string die = "Die";
+        bool isDead;
+
+        private void Start() => animator = GetComponent<Animator>();
 
         public void TakeDamage(float damage)
         {
-            health = Mathf.Max(health - damage, 0);
-            print(health);
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+
+            if (healthPoints == 0)
+                Die();
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+
+            isDead = true;
+            animator.SetTrigger(die);
         }
     }
 }
