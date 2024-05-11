a73039f16ef227234a1a3b7d1b05ee12d6095bab
Kshitijv24 <kshitijv24@gmail.com>
2024-05-11 09:33:57 +0530

    Added Waypoints for Enemy Patrol Path and ability to visualize them in unity scene view.

---
+++ RPG-Game-Project/Assets/Scripts/PatrolPath.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float wayPointGizmosRadios = 0.3f;
+
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), wayPointGizmosRadios);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+            }
+        }
+
+        private int GetNextIndex(int i)
+        {
+            if(i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int i) => transform.GetChild(i).position;
+    }
+}
