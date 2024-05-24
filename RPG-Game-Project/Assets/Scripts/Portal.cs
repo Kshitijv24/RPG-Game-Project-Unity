a59dd74cccfaae72016ff3182771b598e1f048ea
Kshitijv24 <kshitijv24@gmail.com>
2024-05-24 13:23:40 +0530

    Spawning the player at other scene portal spawn point.

--- RPG-Game-Project/Assets/Scripts/Portal.cs
+++ RPG-Game-Project/Assets/Scripts/Portal.cs
@@ -1,4 +1,5 @@
 using RPG.Control;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,13 +10,45 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other)
         {
             PlayerController player = other.GetComponent<PlayerController>();
-            
-            if(player != null)
-                SceneManager.LoadScene(sceneToLoad);
+
+            if (player != null)
+                StartCoroutine(SceneTransition());
+        }
+
+        private IEnumerator SceneTransition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            SetPlayerSpawnPoint(otherPortal);
+
+            print("Scene Loaded");
+            Destroy(gameObject);
+        }
+
+        private void SetPlayerSpawnPoint(Portal otherPortal)
+        {
+            PlayerController player = FindObjectOfType<PlayerController>();
+
+            player.transform.position = otherPortal.spawnPoint.position;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) 
+                    continue;
+
+                return portal;
+            }
+            return null;
         }
     }
 }
