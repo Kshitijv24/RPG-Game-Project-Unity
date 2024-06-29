d2e03226f093f50967ba28733cd88548688bc402
Kshitijv24 <kshitijv24@gmail.com>
2024-06-29 10:39:44 +0530

    Added Weapon Pickups Respawning.
    now weapons respawn after some time when we pick them up.

--- RPG-Game-Project/Assets/Scripts/WeaponPickup.cs
+++ RPG-Game-Project/Assets/Scripts/WeaponPickup.cs
@@ -1,4 +1,5 @@
 using RPG.Control;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,8 +9,12 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon;
+        [SerializeField] float respawnTime = 5f;
 
         PlayerController player;
+        Collider collider;
+
+        private void Awake() => collider = GetComponent<Collider>();
 
         private void OnTriggerEnter(Collider other)
         {
@@ -18,8 +23,23 @@ namespace RPG.Combat
             if(player != null)
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
             }
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickUp(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickUp(true);
+        }
+
+        private void ShowPickUp(bool shouldShow)
+        {
+            collider.enabled = shouldShow;
+
+            foreach (Transform child in transform)
+                child.gameObject.SetActive(shouldShow);
         }
     }
 }
