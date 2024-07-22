584ea03f1aaf3da70eb9f6f2acce267fb05717ea
Kshitij Vashishtha <kshitijv24@gmail.com>
2024-07-22 11:25:44 +0530

    Added Player Level Display UI.

---
+++ RPG-Game-Project/Assets/Scripts/PlayerLevelDisplay.cs
@@ -0,0 +1,21 @@
+using RPG.Control;
+using RPG.Stats;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerLevelDisplay : MonoBehaviour
+{
+    BaseStats baseStats;
+    TextMeshProUGUI levelText;
+
+    private void Awake()
+    {
+        baseStats = FindObjectOfType<PlayerController>().GetComponent<BaseStats>();
+        levelText = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update() => levelText.text = String.Format("{0:0}", baseStats.GetLevel());
+}
